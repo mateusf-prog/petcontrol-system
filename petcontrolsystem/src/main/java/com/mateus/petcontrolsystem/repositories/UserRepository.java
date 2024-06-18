4a2172c81701447057edb59adfcf3b189154cfe1
mateusfprog <mateus102006@hotmail.com>
2024-06-18 20:45:00 -0300

    created userRegister method in the UserService and updated UserRepository

--- petcontrolsystem/src/main/java/com/mateus/petcontrolsystem/repositories/UserRepository.java
+++ petcontrolsystem/src/main/java/com/mateus/petcontrolsystem/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.mateus.petcontrolsystem.models.User;
 import com.mateus.petcontrolsystem.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email OR u.cpfCnpj = :cpfCnpj")
+    List<User> findByEmailOrCpfCnpj(@Param("email") String email, @Param("cpfCnpj") String cpfCnpj);
 
     @Query(nativeQuery = true, value = """
             SELECT tb_users.email AS username, tb_users.password, tb_roles.id AS roleId, tb_roles.authority
--- petcontrolsystem/src/main/java/com/mateus/petcontrolsystem/services/UserService.java
+++ petcontrolsystem/src/main/java/com/mateus/petcontrolsystem/services/UserService.java
@@ -1,24 +1,31 @@
 package com.mateus.petcontrolsystem.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mateus.petcontrolsystem.dto.UserLoginDTO;
+import com.mateus.petcontrolsystem.dto.UserRegisterDTO;
+import com.mateus.petcontrolsystem.dto.UserRegisterReturnDTO;
 import com.mateus.petcontrolsystem.models.User;
 import com.mateus.petcontrolsystem.repositories.UserRepository;
+import com.mateus.petcontrolsystem.services.exceptions.EntityAlreadyExistsException;
 import com.mateus.petcontrolsystem.services.exceptions.ResourceNotFoundException;
 import com.mateus.petcontrolsystem.services.exceptions.InvalidPasswordException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final ObjectMapper mapper;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository repository, ObjectMapper mapper, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.mapper = mapper;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -37,10 +44,23 @@ public class UserService {
         return new UserLoginDTO(user);
     }
 
+    public UserRegisterReturnDTO userRegister(UserRegisterDTO dto) {
+        List<User> existingUsers = repository.findByEmailOrCpfCnpj(dto.getEmail(), dto.getCpfCnpj());
 
+        for (User existingUser : existingUsers) {
+            if (existingUser.getEmail().equals(dto.getEmail())) {
+                throw new EntityAlreadyExistsException("USUÁRIO COM MESMO EMAIL JÁ CADASTRADO");
+            }
+            if (existingUser.getCpfCnpj().equals(dto.getCpfCnpj())) {
+                throw new EntityAlreadyExistsException("USUÁRIO COM MESMO CPF/CNPJ JÁ CADASTRADO");
+            }
+        }
 
-
-
+        User user = mapper.convertValue(dto, User.class);
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user = repository.save(user);
+        return new UserRegisterReturnDTO(user);
+    }
 
 
 //    @Override
