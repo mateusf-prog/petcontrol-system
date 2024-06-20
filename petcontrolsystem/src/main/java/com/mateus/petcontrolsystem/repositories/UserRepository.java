4ea4d1257e10f5032dea02ac210b7d895fb9d928
mateusfprog <mateus102006@hotmail.com>
2024-06-19 23:15:35 -0300

    updated UserRepository and deleted UserDetailsProjection

--- petcontrolsystem/src/main/java/com/mateus/petcontrolsystem/repositories/UserRepository.java
+++ petcontrolsystem/src/main/java/com/mateus/petcontrolsystem/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.mateus.petcontrolsystem.repositories;
 
 import com.mateus.petcontrolsystem.models.User;
-import com.mateus.petcontrolsystem.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,13 +14,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email OR u.cpfCnpj = :cpfCnpj")
     List<User> findByEmailOrCpfCnpj(@Param("email") String email, @Param("cpfCnpj") String cpfCnpj);
-
-    @Query(nativeQuery = true, value = """
-            SELECT tb_users.email AS username, tb_users.password, tb_roles.id AS roleId, tb_roles.authority
-            FROM tb_users
-            INNER JOIN tb_user_role ON tb_users.id = tb_user_role.user_id
-            INNER JOIN tb_roles ON tb_roles.id = tb_user_role.role_id
-            WHERE tb_users.email = :email
-        """)
-    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 }
