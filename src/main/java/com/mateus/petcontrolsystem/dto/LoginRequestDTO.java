8dd0d09bf99aefef0d9babb12b717b3d855f2d97
mateusfprog <mateus102006@hotmail.com>
2024-06-24 11:20:56 -0300

    added NotBlank annotation in the LoginRequestDTO

--- src/main/java/com/mateus/petcontrolsystem/dto/LoginRequestDTO.java
+++ src/main/java/com/mateus/petcontrolsystem/dto/LoginRequestDTO.java
@@ -1,12 +1,15 @@
 package com.mateus.petcontrolsystem.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record LoginRequestDTO(
 
+        @NotBlank(message = "Email cannot be null")
         @Email(message = "Invalid Email")
         String email,
+        @NotBlank(message = "Password cannot be white or null ")
         @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$", message = "Password must have at least 8 characters, " +
                 "one uppercase letter, one lowercase letter and one number. Ex: Password123")
         String password)
