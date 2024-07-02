af17b487ed7cf032cfa76d4b91ec16ea2035fc8e
mateusfprog <mateus102006@hotmail.com>
2024-07-02 17:22:32 -0300

    refactoring integration test UserRepositoryTest

--- src/test/java/com/mateus/petcontrolsystem/models/UserRepositoryTest.java
+++ src/test/java/com/mateus/petcontrolsystem/models/UserRepositoryTest.java
@@ -9,9 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Optional;
 
+/**
+ * This class tests all methods in the UserRepository that using in the UserService
+ */
 @DataJpaTest
 public class UserRepositoryTest {
 
@@ -22,7 +26,7 @@ public class UserRepositoryTest {
     private TestEntityManager testEntityManager;
 
     @Test
-    public void login_WithValidData_ReturnsUser() {
+    public void findByEmail_WithExistsUserByEmail_ReturnsUser() {
 
         var validUser = UserConstants.getValidUser();
         testEntityManager.persistAndFlush(validUser);
@@ -30,28 +34,21 @@ public class UserRepositoryTest {
         Optional<User> foundUser = repository.findByEmail(validUser.getEmail());
 
         assertThat(foundUser).isNotEmpty();
+        assertThat(foundUser.get().getEmail()).isEqualTo(validUser.getEmail());
     }
 
     @Test
-    public void login_WithInvalidData_ReturnsThrowsException() {
-
-        assertThatThrownBy(() -> repository.save(new User())).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    public void login_WithAlreadyUser_ReturnsThrowsException() {
+    public void findByEmail_WithNonExistentUserByEmail_ReturnsUser() {
 
         var validUser = UserConstants.getValidUser();
 
-        testEntityManager.persistFlushFind(validUser);
-        testEntityManager.detach(validUser);
-        validUser.setId(null);
+        var sut = repository.findByEmail(validUser.getEmail());
 
-        assertThatThrownBy(() -> repository.save(validUser)).isInstanceOf(RuntimeException.class);
+        assertThat(sut).isEqualTo(Optional.empty());
     }
 
     @Test
-    public void register_WithValidData_ReturnsUser() {
+    public void save_WithValidData_ReturnsUser() {
 
         var validUser = UserConstants.getValidUser();
 
@@ -62,7 +59,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void register_WithInvalidData_ReturnsThrowsException() {
+    public void save_WithInvalidData_ReturnsThrowsException() {
 
         var invalidUser = UserConstants.getInvalidUser();
 
@@ -70,7 +67,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void register_WithAlreadyUserByEmail_ReturnsThrowsException() {
+    public void save_WithAlreadyUserByEmail_ReturnsThrowsException() {
 
         var validUser = UserConstants.getValidUser();
 
@@ -96,6 +93,21 @@ public class UserRepositoryTest {
         assertThat(sut).isNotNull();
         assertThat(sut.getName()).isEqualTo(userFound.getName());
         assertThat(sut.getPhone()).isEqualTo(userFound.getPhone());
+    }
+
+    @Test
+    public void update_WithInvalidData_ReturnThrowsException() {
+
+        var validUser = UserConstants.getValidUser();
+        testEntityManager.persistAndFlush(validUser);
+
+        var userFound = repository.findByEmailOrCpfCnpj(validUser.getEmail(), validUser.getCpfCnpj());
+        userFound.setName(null); // invalid name
+
+        assertThatThrownBy(() -> {
+            repository.save(userFound);
+            testEntityManager.flush();
+        }).isInstanceOf(RuntimeException.class);
     }
 
 }
