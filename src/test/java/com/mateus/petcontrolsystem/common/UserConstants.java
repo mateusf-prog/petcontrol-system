3c6431d59dd70f1c392f7548891c4512ff240011
mateusfprog <mateus102006@hotmail.com>
2024-07-03 10:41:17 -0300

    update UserConstants

--- src/test/java/com/mateus/petcontrolsystem/common/UserConstants.java
+++ src/test/java/com/mateus/petcontrolsystem/common/UserConstants.java
@@ -9,8 +9,8 @@ import java.time.LocalDate;
 import java.util.stream.Stream;
 
 /**
- * This class provides constants for User entity and DTOs related tests.
- * All the Objects here use the same values
+ * This class provides constants for User entity. Used by AuthControllerTest, UserServiceTest and UserRepositoryTest
+ * All the Objects here use the same values for his fields
  */
 public class UserConstants {
 
@@ -36,8 +36,13 @@ public class UserConstants {
         return user;
     }
 
-    public static User getInvalidUser() {
-        return new User();
+    // these objects are used in UserServiceTest
+    public static LoginRequestDTO getValidLoginRequestDTO() {
+        return new LoginRequestDTO("jon.doe@hotmail.com", "Password1234");
+    }
+
+    public static UserAccessDataRequestDTO getValidUserAccessDataDTO() {
+        return new UserAccessDataRequestDTO("jon.doe@hotmail.com", "Password1234", "NewPassword4321");
     }
 
     public static RegisterRequestDTO getValidRegisterRequestDTO() {
@@ -46,37 +51,88 @@ public class UserConstants {
                 new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"));
     }
 
-    public static RegisterRequestDTO getInvalidRegisterRequestDTO() {
-        return new RegisterRequestDTO(null, null, null, null,
-                null, null,
-                new AddressDTO(null, null, null, null, null, null));
-    }
-
-    public static RegisterRequestDTO getEmptyFieldsRegisterRequestDTO() {
-        return new RegisterRequestDTO("", "", "", "",
-                LocalDate.of(0, 0, 0), "",
-                new AddressDTO("", "", "", "", "", ""));
-    }
-
     public static UpdateUserDTO getValidUserUpdateDto() {
         return new UpdateUserDTO("Jon Doe", "1299999999", "50108953076",
                 new AddressDTO("Rua José Domingues", "170", "Centro", "São José dos Campos", "SP", "12212700"));
     }
 
-    public static UpdateUserDTO getInValidUserUpdateDto() {
-        return new UpdateUserDTO(null, null, null,
-                new AddressDTO(null, null, null, null, null, null));
+
+    // these objects are used in AuthControllerTest
+    public static Stream<Arguments> provideInvalidUserUpdateDTO() {
+        return Stream.of(
+                Arguments.of(new UpdateUserDTO(null, "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", null, "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", null,
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO(null, "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", null, "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", null, "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", null, "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", null, "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", null))),
+                Arguments.of(new UpdateUserDTO("", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jo", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "invalid-phone", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "invalid-cpf",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "", "170", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "", "Jardim Telespark", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "", "São José dos Campos", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "", "SP"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("12212700", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "invalid-uf"))),
+                Arguments.of(new UpdateUserDTO("Jon Doe", "12991978448", "50108953076",
+                        new AddressDTO("invalid-cep", "Rua das Araras", "170", "Jardim Telespark", "São José dos Campos", "SP")))
+
+        );
     }
 
-    public static UserAccessDataRequestDTO getValidUserAccessDataDTO() {
-        return new UserAccessDataRequestDTO("jon.doe@hotmail.com", "Password1234", "NewPassword4321");
+    public static Stream<Arguments> provideInvalidLoginRequestDTO() {
+        return Stream.of(
+                Arguments.of(new LoginRequestDTO(null, "Password@1234")),
+                Arguments.of(new LoginRequestDTO("jon.doe@hotmail.com", null)),
+                Arguments.of(new LoginRequestDTO("", "Password@1234")),
+                Arguments.of(new LoginRequestDTO("jon.doe@hotmail.com", "")),
+                Arguments.of(new LoginRequestDTO("invalid-email", "Password@1234")),
+                Arguments.of(new LoginRequestDTO("jon.doe@hotmail.com", "invalidpasswordpattern"))
+        );
     }
 
-    public static LoginRequestDTO getValidLoginRequestDTO() {
-        return new LoginRequestDTO("jon.doe@hotmail.com", "Password1234");
+    public static Stream<Arguments> provideInvalidUserAccessDataRequestDTO() {
+        return Stream.of(
+                Arguments.of(new UserAccessDataRequestDTO(null, "Password@1234", "Password@1234")),
+                Arguments.of(new UserAccessDataRequestDTO("jon.doe@hotmail.com", null, "Password@1234")),
+                Arguments.of(new UserAccessDataRequestDTO("jon.doe@hotmail.com", "Password@1234", null)),
+                Arguments.of(new UserAccessDataRequestDTO("", "Password@1234", "Password@1234")),
+                Arguments.of(new UserAccessDataRequestDTO("jon.doe@hotmail.com", "", "Password@1234")),
+                Arguments.of(new UserAccessDataRequestDTO("jon.doe@hotmail.com", "Password@1234", "")),
+                Arguments.of(new UserAccessDataRequestDTO("invalid-email", "Password@1234", "Password@1234")),
+                Arguments.of(new UserAccessDataRequestDTO("jon.doe@hotmail.com", "invalidpasswordpattern", "Password@1234")),
+                Arguments.of(new UserAccessDataRequestDTO("jon.doe@hotmail.com","Password@1234", "invalidpasswordpattern"))
+        );
     }
 
-    // test different scenarios for User
     public static Stream<Arguments> provideInvalidRegisterRequestDTO() {
         return Stream.of(
                 Arguments.of(new RegisterRequestDTO(null, "jon.doe@hotmail.com", "50108953076", "12997825148", LocalDate.of(1958, 6, 25), "Password1234",
