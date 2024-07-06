e2e5aa17a8d2500d0d78d406c74c29040958cc6a
mateus <mateus102006@hotmail.com>
2024-07-05 21:23:15 -0300

    create a new security config (profile=test) that permit all access in the all endpoints

---
+++ src/main/java/com/mateus/petcontrolsystem/infra/security/TestSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.mateus.petcontrolsystem.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("test")
+@Configuration
+@EnableWebSecurity
+public class TestSecurityConfig {
+
+    @Bean
+    @Profile("test")
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
+
