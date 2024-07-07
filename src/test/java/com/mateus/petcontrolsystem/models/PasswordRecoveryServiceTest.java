04e49def0c33ad2a56492dca45da9605a3a37292
mateus <mateus102006@hotmail.com>
2024-07-06 21:19:07 -0300

    create four more tests in the PasswordRecoveryServiceTest

--- src/test/java/com/mateus/petcontrolsystem/models/PasswordRecoveryServiceTest.java
+++ src/test/java/com/mateus/petcontrolsystem/models/PasswordRecoveryServiceTest.java
@@ -1,14 +1,20 @@
 package com.mateus.petcontrolsystem.models;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.mateus.petcontrolsystem.common.PasswordRecoveryConstants;
+import com.mateus.petcontrolsystem.common.UserConstants;
+import com.mateus.petcontrolsystem.dto.password.CodeReceivedEmailResponseDTO;
 import com.mateus.petcontrolsystem.infra.security.TokenService;
 import com.mateus.petcontrolsystem.repositories.PasswordRecoveryRepository;
 import com.mateus.petcontrolsystem.repositories.UserRepository;
 import com.mateus.petcontrolsystem.services.EmailService;
 import com.mateus.petcontrolsystem.services.PasswordRecoveryService;
+import com.mateus.petcontrolsystem.services.exceptions.InvalidProcessRecoveryPasswordException;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,6 +24,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
+/**
+ * This class contains unit tests for the PasswordRecoveryService class.
+ * It uses UserConstants to create a valid User entity.
+ */
 @ExtendWith(MockitoExtension.class)
 public class PasswordRecoveryServiceTest {
 
@@ -51,4 +61,59 @@ public class PasswordRecoveryServiceTest {
 
         verify(emailService).sendCodeToEmail(eq(fixedCode), eq(validRequestBody.email()));
     }
+
+    @Test
+    public void sendCodeToEmail_WithNonExistingUserByEmail_ThrowsException() {
+
+        var invalidDTO = PasswordRecoveryConstants.getInvalidEmailToRecoverPasswordDTO();
+
+        assertThatThrownBy(() -> service.sendCodeToEmail(invalidDTO)).isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    public void validateCodeReceivedInEmail_WithValidCode_ReturnsCodeReceivedEmailResponseDTO() {
+
+        var validRequestBody = PasswordRecoveryConstants.getValidCodeReceivedEmailRequestDTO();
+        var validUser = UserConstants.getValidUser();
+        var mockPasswordRecovery = PasswordRecoveryConstants.getValidPasswordEntity();
+        var expectedResponse = new CodeReceivedEmailResponseDTO("generated-token-test");
+
+        when(userRepository.findByEmail(validRequestBody.email())).thenReturn(Optional.of(validUser));
+        when(passwordRecoveryRepository.findByUserEmail(validUser.getEmail())).thenReturn(Optional.of(mockPasswordRecovery));
+        when(tokenService.generateTemporaryTokenToRecoveryPassword(validUser)).thenReturn("generated-token-test");
+        when(passwordRecoveryRepository.save(mockPasswordRecovery)).thenReturn(mockPasswordRecovery);
+
+        PasswordRecoveryService spyService = spy(service);
+        doNothing().when(spyService).validateCodeReceived(validRequestBody, mockPasswordRecovery);
+
+        var sut = spyService.validateCodeReceivedInEmail(validRequestBody);
+
+        assertThat(sut).isEqualTo(expectedResponse);
+    }
+
+    @Test
+    public void validateCodeReceivedInEmail_WithNonExistingUserByEmail_ThrowsException() {
+
+        var validRequestBody = PasswordRecoveryConstants.getValidCodeReceivedEmailRequestDTO();
+
+        when(userRepository.findByEmail(validRequestBody.email())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> service.validateCodeReceivedInEmail(validRequestBody)).isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    public void validateCodeReceivedInEmail_WithNonExistentRequest_ThrowsException() {
+
+        var validRequestBody = PasswordRecoveryConstants.getValidCodeReceivedEmailRequestDTO();
+        var validUser = UserConstants.getValidUser();
+        validUser.setEmail(validRequestBody.email());
+
+        when(userRepository.findByEmail(validRequestBody.email())).thenReturn(Optional.of(validUser));
+        when(passwordRecoveryRepository.findByUserEmail(validRequestBody.email())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> service.validateCodeReceivedInEmail(validRequestBody)).isInstanceOf(InvalidProcessRecoveryPasswordException.class);
+    }
+
+    //todo: missing setNewUserPassword, generateCodeToSendToEmail ans validateCodeReceived methods
+
 }
