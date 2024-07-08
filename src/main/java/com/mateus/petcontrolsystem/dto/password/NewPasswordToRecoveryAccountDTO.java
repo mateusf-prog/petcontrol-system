efafc64b8bf5cb755fb6c113928efbfb9b82f677
mateus <mateus102006@hotmail.com>
2024-07-08 20:34:30 -0300

    added an annotation in the NewPasswordToRecoveryAccountDTO

--- src/main/java/com/mateus/petcontrolsystem/dto/password/NewPasswordToRecoveryAccountDTO.java
+++ src/main/java/com/mateus/petcontrolsystem/dto/password/NewPasswordToRecoveryAccountDTO.java
@@ -1,10 +1,13 @@
 package com.mateus.petcontrolsystem.dto.password;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record NewPasswordToRecoveryAccountDTO(
 
+        @NotBlank
+        @Email
         String email,
         @NotBlank(message = "Password cannot be null")
         @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$", message = "Password must have at least 8 characters, " +
