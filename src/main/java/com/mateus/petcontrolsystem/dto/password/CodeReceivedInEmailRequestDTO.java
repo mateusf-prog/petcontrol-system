163976412327dd67962c462fdd9e33881d3544e5
mateus <mateus102006@hotmail.com>
2024-07-08 18:28:23 -0300

    added an annotation in the code field on the CodeReceivedInEmailRequestDTO

--- src/main/java/com/mateus/petcontrolsystem/dto/password/CodeReceivedInEmailRequestDTO.java
+++ src/main/java/com/mateus/petcontrolsystem/dto/password/CodeReceivedInEmailRequestDTO.java
@@ -1,11 +1,17 @@
 package com.mateus.petcontrolsystem.dto.password;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.NumberFormat;
 
 public record CodeReceivedInEmailRequestDTO(
 
+        @NotBlank
+        @Email
         String email, // this field will arrive implicitly
         @NotBlank(message = "Code cannot be null")
+        @Pattern(regexp = "\\d+", message = "The field must contain only numbers.")
         @Size(min = 5, max = 5, message = "Code must be 5 numbers")
         String code) {}
