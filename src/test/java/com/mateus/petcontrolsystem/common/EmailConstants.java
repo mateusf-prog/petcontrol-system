acc778cdd6d8d8d0e9ba52e23029958dfcc610bd
mateus <mateus102006@hotmail.com>
2024-07-11 00:11:37 -0300

    create EmailIntegrationTest and more methods in EmailConstants

--- src/test/java/com/mateus/petcontrolsystem/common/EmailConstants.java
+++ src/test/java/com/mateus/petcontrolsystem/common/EmailConstants.java
@@ -22,4 +22,37 @@ public class EmailConstants {
         emailToSend.setText(message);
         return  emailToSend;
     }
+
+    public static SimpleMailMessage getSimpleMailMessageWithInvalidSender() {
+        String message = "Seu código de verificação é " + getCode() + "\nSeu código tem duração de 10 minutos.";
+
+        SimpleMailMessage emailToSend = new SimpleMailMessage();
+        emailToSend.setTo(getValidEmail());
+        emailToSend.setFrom("invalid sender");
+        emailToSend.setSubject("Código de verificação - PetControl System");
+        emailToSend.setText(message);
+        return  emailToSend;
+    }
+
+    public static SimpleMailMessage getSimpleMailMessageWithInvalidReceiver() {
+        String message = "Seu código de verificação é " + getCode() + "\nSeu código tem duração de 10 minutos.";
+
+        SimpleMailMessage emailToSend = new SimpleMailMessage();
+        emailToSend.setTo("invalid receiver");
+        emailToSend.setFrom("noreply@petcontrolsystem");
+        emailToSend.setSubject("Código de verificação - PetControl System");
+        emailToSend.setText(message);
+        return  emailToSend;
+    }
+
+    public static SimpleMailMessage getSimpleMailMessageWithInvalidText() {
+        String message = null;
+
+        SimpleMailMessage emailToSend = new SimpleMailMessage();
+        emailToSend.setTo(getValidEmail());
+        emailToSend.setFrom("noreply@petcontrolsystem");
+        emailToSend.setSubject("Código de verificação - PetControl System");
+        emailToSend.setText(message);
+        return  emailToSend;
+    }
 }
--- src/test/java/com/mateus/petcontrolsystem/models/EmailIntegrationTest.java
+++ src/test/java/com/mateus/petcontrolsystem/models/EmailIntegrationTest.java
@@ -1,6 +1,72 @@
 package com.mateus.petcontrolsystem.models;
 
+import com.mateus.petcontrolsystem.common.EmailConstants;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+/**
+ * This class is disable because the host smtp is free, therefore it have a monthly limit of requests.
+ * NOTE: This affirmation its only this host configuration
+ */
+
+@Disabled
+@SpringBootTest
 public class EmailIntegrationTest {
 
-    //todo: implement
+    @Value("sandbox.smtp.mailtrap.io")
+    private String host;
+    @Value("2525")
+    private Integer port;
+    @Value("${SPRING_MAIL_USERNAME_TEST}")
+    private String username;
+    @Value("${SPRING_MAIL_PASSWORD_TEST}")
+    private String password;
+
+    public JavaMailSender setup() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        return mailSender;
+    }
+
+    @Test
+    public void sendEmailTest_WithValidData_Returns200OK() {
+
+        var validData = EmailConstants.getValidSimpleMailMessage();
+
+        Assertions.assertDoesNotThrow(() -> setup().send(validData));
+    }
+
+    @Test
+    public void sendEmailTest_WithInvalidSender_ThrowsMailException() {
+
+        var withInvalidSender = EmailConstants.getSimpleMailMessageWithInvalidSender();
+
+        Assertions.assertThrows(MailException.class, () -> setup().send(withInvalidSender));
+    }
+
+    @Test
+    public void sendEmailTest_WithInvalidReceiver_ThrowsMailException() {
+
+        var withInvalidReceiver = EmailConstants.getSimpleMailMessageWithInvalidReceiver();
+
+        Assertions.assertThrows(MailException.class, () -> setup().send(withInvalidReceiver));
+    }
+
+    @Test
+    public void sendEmailTest_WithInvalidText_ThrowsMailException() {
+
+        var withInvalidText = EmailConstants.getSimpleMailMessageWithInvalidText();
+
+        Assertions.assertThrows(MailException.class, () -> setup().send(withInvalidText));
+    }
+
 }
