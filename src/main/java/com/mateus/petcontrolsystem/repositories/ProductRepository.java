cd5e90d7138eb0369c91a284f673a893b4ddcb9f
mateus <mateus102006@hotmail.com>
2024-07-16 19:47:03 -0300

    refactor ProductRepository

--- src/main/java/com/mateus/petcontrolsystem/repositories/ProductRepository.java
+++ src/main/java/com/mateus/petcontrolsystem/repositories/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.mateus.petcontrolsystem.repositories;
 
 import com.mateus.petcontrolsystem.models.Product;
-import com.mateus.petcontrolsystem.models.enums.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,9 +10,5 @@ import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT obj FROM Product obj " +
-            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
-    Page<Product> searchByName(String name, Pageable pageable);
-
-    Optional<Product> findByNameAndSupplierAndCategory(String name, String supplier, Category category);
+    Optional<Product> findByNameAndSupplier(String name, String supplier);
 }
