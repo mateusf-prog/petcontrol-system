2d2bc9dade0bf71fda6f581fb85ec4d1d546d508
mateus <mateus102006@hotmail.com>
2024-07-18 21:23:54 -0300

    create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_services (name, small_dog_price, medium_size_dog_price, big_dog_price) VALUES ('Tosa Higiênica', 50.00, 60.00, 70.00);
+INSERT INTO tb_services (name, small_dog_price, medium_size_dog_price, big_dog_price) VALUES ('Tosa Tesoura', 80.00, 90.00, 100.00);
+INSERT INTO tb_services (name, small_dog_price, medium_size_dog_price, big_dog_price) VALUES ('Tosa Máquina', 120.00, 150.00, 180.00);
+INSERT INTO tb_services (name, small_dog_price, medium_size_dog_price, big_dog_price) VALUES ('Táxi Dog', 30.00, 40.00, 50.00);
+
+INSERT INTO tb_products(name, category, supplier, price, stock, description) VALUES ('Coleira', 'ACCESSORY', 'empresa1', 30.00, 50, 'Coleira simples colorida');
+INSERT INTO tb_products(name, category, supplier, price, stock, description) VALUES ('Vacina Antirábica', 'VACCINE', 'Labovet', 60.00, 40, 'vacina de raiva para pets');
+INSERT INTO tb_products(name, category, supplier, price, stock, description) VALUES ('Antipulgas', 'OTHER', 'Labovet', 50.00, 40, 'Antipulgas para cachorros pequenos');
+
+INSERT INTO tb_customers(name, email, phone, birth_date, cpf_cnpj, zip_code, street, number, district, city, uf) VALUES ('Carlos', 'carlos@email.com', '11912345678', '1980-06-15', '974646546413', '12212700', 'Rua das Flores', '123', 'Centro', 'São José dos Campos', 'SP');
+INSERT INTO tb_customers(name, email, phone, birth_date, cpf_cnpj, zip_code, street, number, district, city, uf) VALUES ('Ana', 'ana@email.com', '11923456789', '1990-07-25', '64654649987', '35748000', 'Rua das Palmeiras', '456', 'Centro', 'Campinas', 'SP');
+INSERT INTO tb_customers(name, email, phone, birth_date, cpf_cnpj, zip_code, street, number, district, city, uf) VALUES ('Pedro', 'pedro@email.com', '11934567890', '2000-08-05', '65432198745', '12345678', 'Rua das Avenidas', '789', 'Centro', 'Paraisópolis', 'SP');
