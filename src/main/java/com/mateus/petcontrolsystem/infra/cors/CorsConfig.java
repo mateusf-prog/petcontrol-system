ca1a8faa97c30a448f3318f534d03ad37ae5bc6e
mateus <mateus102006@hotmail.com>
2024-08-14 13:54:02 -0300

    create new methods ServiceRepository

--- src/main/java/com/mateus/petcontrolsystem/infra/cors/CorsConfig.java
+++ src/main/java/com/mateus/petcontrolsystem/infra/cors/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // URL do seu aplicativo React
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
     }
--- src/main/java/com/mateus/petcontrolsystem/repositories/ServiceRepository.java
+++ src/main/java/com/mateus/petcontrolsystem/repositories/ServiceRepository.java
@@ -3,5 +3,9 @@ package com.mateus.petcontrolsystem.repositories;
 import com.mateus.petcontrolsystem.models.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ServiceRepository extends JpaRepository<Service, Long> {
+
+    Optional<Service> findByName(String name);
 }
